574957948cbe1449fd623a9d91c7be6ab9d90a9d
abbasskahnn <abbasskahnn@gmail.com>
2024-03-02 00:45:57 +0000

    added recipe data usingevent and property binding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,11 +7,16 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes:Recipe[] = [
     new Recipe('A Test Recipe','This is simply a test',
     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxB5a-TRNTZJ_snSbtFg3v2FacQPCgP3g88A&usqp=CAU'),
-    new Recipe('A Test Recipe','This is simply a test',
+    new Recipe('Another Test Recipe','This is simply a test',
     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxB5a-TRNTZJ_snSbtFg3v2FacQPCgP3g88A&usqp=CAU')
   ];
+
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
+  }
 
 }
